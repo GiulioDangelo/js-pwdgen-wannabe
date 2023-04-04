59cc24dafc1af8778daa2400f87f04b5ea857998
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-04 16:29:04 +0200

    add number

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const Name = prompt("Come ti chiami?");
 const lastName = prompt("Qual è il tuo cognome?");
 const color = prompt("Qual è il tuo colore preferito?");
 
-let password = userName + lastName + color;  
+let password = Name + lastName + color;  
 
 
 let elementSpan = document.getElementById("result");
 
-elementSpan.innerHTML = password;
+elementSpan.innerHTML = password + "23";
