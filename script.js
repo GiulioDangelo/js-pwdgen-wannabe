03d0caa85f975bc63f13697c49183024388c99a8
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-04 16:56:54 +0200

    add font size

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const Name = prompt("Come ti chiami?");
 const lastName = prompt("Qual è il tuo cognome?");
 const color = prompt("Qual è il tuo colore preferito?");
 
-let password = Name + lastName + color;  
-
+let password = Name + lastName + color;
 
 let elementSpan = document.getElementById("result");
 
